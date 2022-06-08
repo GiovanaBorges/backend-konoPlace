dcbaa14fae0df08a87c6123a0695422c2f594747
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-06-08 12:54:44 -0300

    inital commit

---
+++ konoplace/src/main/java/com/konoPlace/konoplace/controllers/UserController.java
@@ -0,0 +1,11 @@
+package com.konoPlace.konoplace.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/user")
+@CrossOrigin("*")
+public class UserController {
+
+
+}

f4a25a9052c930936bbfbb1fc5c32d77888afe54
GiovanaBorges <gigi12345as17@gmail.com>
2022-06-09 04:45:16 -0300

    add reserva basic  crud

---
+++ konoplace/src/main/java/com/konoPlace/konoplace/controllers/ReservaController.java
@@ -0,0 +1,41 @@
+package com.konoPlace.konoplace.controllers;
+
+
+import com.konoPlace.konoplace.models.ReservaModel;
+import com.konoPlace.konoplace.repositories.ReservaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reserva")
+@CrossOrigin("*")
+public class ReservaController {
+
+    //injeção de dependencias
+    @Autowired
+    private ReservaRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<ReservaModel>> getReserva(){
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservaModel> createReserva(@RequestBody ReservaModel reserva){
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(reserva));
+    }
+
+    @PutMapping
+    public ResponseEntity<ReservaModel> EditReserva(@RequestBody ReservaModel reserva){
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(reserva));
+    }
+
+    @DeleteMapping("{id}")
+    public void DeleteReserva(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+}

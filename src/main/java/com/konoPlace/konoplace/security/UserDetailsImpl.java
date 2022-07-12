df08d8928f5d5aa8c12b482d2979fa8ca0583e1e
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-07-12 17:38:34 -0300

    changed details authorities

--- src/main/java/com/konoPlace/konoplace/security/UserDetailsImpl.java
+++ src/main/java/com/konoPlace/konoplace/security/UserDetailsImpl.java
@@ -27,36 +27,36 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }

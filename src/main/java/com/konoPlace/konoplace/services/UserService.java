74408ab2adeca8c60554757bd4647e03474ab3c2
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-07-19 17:06:21 -0300

    changed function login and register

--- src/main/java/com/konoPlace/konoplace/services/UserService.java
+++ src/main/java/com/konoPlace/konoplace/services/UserService.java
@@ -1,12 +1,12 @@
 package com.konoPlace.konoplace.services;
 
 
-import com.konoPlace.konoplace.dto.UserRegisterDTO;
 import com.konoPlace.konoplace.models.UserLogin;
 import com.konoPlace.konoplace.models.UserModel;
 import com.konoPlace.konoplace.repositories.UserRepository;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,14 +14,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.nio.charset.Charset;
 import java.util.Optional;
 
 @Service
 public class UserService {
-
     @Autowired
     private UserRepository userRepo;
+
+    @Autowired
+    private CookieService cookieService;
 
     private String encryptPass(String pass){
         BCryptPasswordEncoder encrypt = new BCryptPasswordEncoder();
@@ -34,46 +38,42 @@ public class UserService {
         return encrypt.matches(newpass,pass);
     }
 
-    private String generateBasicToken(String user , String senha){
-        String structure = user + ":" + senha;
-        byte[] structureBase64 = Base64.encodeBase64(structure.getBytes(Charset.forName("US-ASCII")));
-        return "Basic " + new String(structureBase64);
-    }
-
-    public ModelAndView loginUser(UserLogin userlogin){
+    public ModelAndView loginUser(UserLogin userlogin , HttpServletResponse res){
         Optional<UserModel> userCompare = userRepo.findByEmail(userlogin.getEmail());
-        ModelAndView model = new ModelAndView(); 
-        
-        
+
+        ModelAndView model = new ModelAndView();
+
         if(userCompare.isEmpty()){
+            model.setViewName("index.html");
             throw  new ResponseStatusException(HttpStatus.BAD_REQUEST , "This user does not exists!");
         }else{
             if(comparePass(userlogin.getPass(), userCompare.get().getSenha())){
-                userlogin.setId(userlogin.getId());
-                userlogin.setEmail(userlogin.getEmail());
-                userlogin.setPass(userlogin.getPass());
-                userlogin.setToken(generateBasicToken(userlogin.getEmail(), userlogin.getPass()));
+                Optional<UserModel> user = userRepo.findByEmail(userCompare.get().getEmail());
+
+                String userId = String.valueOf(user.get().getId());
+
+                Cookie cookie = new Cookie("userID", userId);
+                cookie.setMaxAge(7 * 24 * 60 * 60); // expires in 7 days
+                res.addCookie(cookie);
+
                 model.setViewName("home.html");
-                return model;
             }
         }
-        model.setViewName("index.html");
+
         return model;
 
     }
 
 
-    public ResponseEntity<UserModel> registerUser(UserModel newUser){
-                UserModel user = new UserModel();
-                user.setNome(newUser.getNome());
-                user.setCargo(newUser.getCargo());
-                user.setDepartamento(newUser.getDepartamento());
-                user.setTelefone(newUser.getTelefone());
-                user.setSenha(encryptPass(newUser.getSenha()));
-                user.setEmail(newUser.getEmail());
-                user.setFoto(newUser.getFoto());
-                
-                return ResponseEntity.status(200).body(userRepo.save(user));
+    public ModelAndView registerUser(UserModel newUser , HttpServletResponse res){
+                ModelAndView mv = new ModelAndView();
+                userRepo.save(newUser);
+                Optional<UserModel> user = userRepo.findByEmail(newUser.getEmail());
+
+                String userId = String.valueOf(user.get().getId());
+                cookieService.setCookie(res,userId);
+                return mv;
+
     }
 
 }

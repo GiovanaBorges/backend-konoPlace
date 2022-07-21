3544062be7074706f7a360ee897e027402702cef
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-07-21 16:15:56 -0300

    changed method  foro see user info

--- src/main/java/com/konoPlace/konoplace/controllers/UserController.java
+++ src/main/java/com/konoPlace/konoplace/controllers/UserController.java
@@ -5,15 +5,16 @@ import com.konoPlace.konoplace.models.UserModel;
 import com.konoPlace.konoplace.repositories.UserRepository;
 import com.konoPlace.konoplace.services.UserService;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -62,9 +63,29 @@ public class UserController {
     }
     
     @GetMapping("/perfil")
-    public ModelAndView perfilScreen()
+    public ModelAndView perfilScreen(HttpServletRequest request)
     {
         ModelAndView model = new ModelAndView();
+
+        Cookie[] cookie = null;
+        cookie = request.getCookies();
+
+        Optional<Cookie> result =  Arrays.stream(cookie).findFirst();
+        Long id = Long.parseLong(result.get().getValue());
+        Optional<UserModel> user = repository.findById(id);
+
+        UserModel userModel = new UserModel();
+        userModel.setEmail(user.get().getEmail());
+        userModel.setSenha(user.get().getSenha());
+        userModel.setId(user.get().getId());
+        userModel.setReserva(user.get().getReserva());
+        userModel.setCargo(user.get().getCargo());
+        userModel.setDepartamento(user.get().getDepartamento());
+        userModel.setFoto(user.get().getFoto());
+        userModel.setTelefone(user.get().getTelefone());
+        userModel.setNome(user.get().getNome());
+
+        model.addObject("user", userModel);
         model.setViewName("perfil.html");
         return model;
     }
@@ -81,12 +102,34 @@ public class UserController {
         userService.loginUser(user, response);
     }
 
-    @PutMapping
-    public ResponseEntity<UserModel> EditUser(@RequestBody UserModel user){
-        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(user));
+    @PutMapping("/update/{id}")
+    public ModelAndView EditUser(@PathVariable Long id,@ModelAttribute UserModel user ,HttpServletRequest request){
+        ModelAndView mv = new ModelAndView();
+        Cookie[] cookie = null;
+        cookie = request.getCookies();
+
+        //Optional<Cookie> result =  Arrays.stream(cookie).findFirst();
+        //Long idUser = Long.parseLong(result.get().getValue());
+
+        UserModel userModel = new UserModel();
+        userModel.setId(id);
+        userModel.setEmail(user.getEmail());
+        userModel.setSenha(user.getSenha());
+        userModel.setId(user.getId());
+        userModel.setReserva(user.getReserva());
+        userModel.setCargo(user.getCargo());
+        userModel.setDepartamento(user.getDepartamento());
+        userModel.setFoto(user.getFoto());
+        userModel.setTelefone(user.getTelefone());
+        userModel.setNome(user.getNome());
+
+        repository.save(user);
+        mv.setViewName("home.html");
+        return mv;
+
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("/delete/{id}")
     public void DeleteUser(@PathVariable Long id){
         repository.deleteById(id);
     }

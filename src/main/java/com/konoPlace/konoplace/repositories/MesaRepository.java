2b4ad6fcb0a9ca7c70a3915cc2ea432631111b01
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-07-29 17:11:38 -0300

    changed method

--- src/main/java/com/konoPlace/konoplace/repositories/MesaRepository.java
+++ src/main/java/com/konoPlace/konoplace/repositories/MesaRepository.java
@@ -5,10 +5,11 @@ import com.konoPlace.konoplace.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MesaRepository extends JpaRepository<MesaModel,Long> {
     public MesaModel findByName(String name);
-
+    public List<MesaModel> findAll();
 }

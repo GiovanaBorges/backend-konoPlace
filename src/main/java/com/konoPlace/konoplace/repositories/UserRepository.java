b6ad3c86bc382f074916e2f87e716236bde00325
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-07-29 17:11:17 -0300

    changed method

--- src/main/java/com/konoPlace/konoplace/repositories/UserRepository.java
+++ src/main/java/com/konoPlace/konoplace/repositories/UserRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserModel, Long> {
     public UserModel findByEmail(String email);
     public Optional<UserModel> findById(Long id);
+
 }
